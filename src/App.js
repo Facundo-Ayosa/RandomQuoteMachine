f975a3038b6decee44fdf0361a7d434c6301a913
Facundo Ayosa <80654651+Facundo-Ayosa@users.noreply.github.com>
2022-08-09 16:47:42 -0300

    framework mostly done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import fetchquote from './fetch-quotes';
 
 function App() {
+  const [quote, setQuote] = useState();
+
+  const clickHandler = async () => {
+    await fetchquote().then((res) => { console.log(quote); setQuote(res);});
+  }
+  useEffect(() => {
+    if (quote) {
+      fetchquote().then(res => setQuote(res));
+    }
+  })
+  const quoteMachine = () => {
+    let elem = quote
+    return (<><h2>{elem.quote}</h2><h3>{elem.character}</h3><h3>{elem.anime}</h3></>)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="quote">
+        {quoteMachine()}
+        <button onClick={clickHandler}>Get Another Quote</button>
+      </div>
+    </main>
   );
 }
 
---
+++ src/fetch-quotes.js
@@ -0,0 +1,5 @@
+async function fetchQuotes(){
+    //const returnState = await fetch('https://animechan.vercel.app/api/random').then((response) => response.json());
+    //return returnState;
+}
+export default fetchQuotes;
